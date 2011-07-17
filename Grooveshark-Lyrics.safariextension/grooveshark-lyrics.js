c235b782fae27df846826e610385d2b585de8c7d
Lucas Torri <lucastorri@gmail.com>
2011-07-17 13:35:52 -0300

    version 1.0

--- Grooveshark-Lyrics.safariextension/grooveshark-lyrics.js
+++ Grooveshark-Lyrics.safariextension/grooveshark-lyrics.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
 
   if (window.top === window) {
     var injectedScript = document.createElement('script');
-    injectedScript.setAttribute('src', 'http://localhost:8000/client.js'); // 'https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js'
+    injectedScript.setAttribute('src', 'https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js');
     injectedScript.setAttribute('type', 'text/javascript');
     document.head.appendChild(injectedScript);
   }

7b314a7ff1c497c459a02aa5bf12d001eab042e3
Lucas Torri <lucastorri@gmail.com>
2011-07-17 04:56:29 -0300

    formating identation on script

--- Grooveshark-Lyrics.safariextension/grooveshark-lyrics.js
+++ Grooveshark-Lyrics.safariextension/grooveshark-lyrics.js
@@ -1,11 +1,10 @@
 window.addEventListener("load", function() {
 
-if (/*document.URL.match(/grooveshark\.com/) && */window.top === window) {
-  console.log('injecting');
-  var injectedScript = document.createElement('script');
-  injectedScript.setAttribute('src', 'http://localhost:8000/client.js'); // 'https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js'
-  injectedScript.setAttribute('type', 'text/javascript');
-  document.head.appendChild(injectedScript);
-}
+  if (window.top === window) {
+    var injectedScript = document.createElement('script');
+    injectedScript.setAttribute('src', 'http://localhost:8000/client.js'); // 'https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js'
+    injectedScript.setAttribute('type', 'text/javascript');
+    document.head.appendChild(injectedScript);
+  }
 
 }, false);

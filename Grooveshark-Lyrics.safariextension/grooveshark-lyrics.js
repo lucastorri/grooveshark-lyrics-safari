d795c6ebe531b84fb9ce5d27dd9dbe26c3d8b44c
Lucas Torri <lucastorri@gmail.com>
2011-07-18 13:54:09 -0300

    putting function on load page event only if that's the main window, not the opposite

--- Grooveshark-Lyrics.safariextension/grooveshark-lyrics.js
+++ Grooveshark-Lyrics.safariextension/grooveshark-lyrics.js
@@ -1,10 +1,13 @@
-window.addEventListener("load", function() {
+if (window.top === window) {
 
-  if (window.top === window) {
+  window.addEventListener("load", function() {
+
     var injectedScript = document.createElement('script');
     injectedScript.setAttribute('src', 'https://raw.github.com/nettofarah/grooveshark-lyrics/client/client.js');
     injectedScript.setAttribute('type', 'text/javascript');
     document.head.appendChild(injectedScript);
-  }
 
-}, false);
+  }, false);
+
+}
+
